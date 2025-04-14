f08870a54f5bb79449697c66d9bf995a9f966b4a
josefina-sfilio <jsfilioglassmann@mail.austral.edu.ar>
2025-04-14 15:41:20 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -3,4 +3,4 @@ def leap_year():
     if ((año % 4 == 0) and (año % 100 != 0)) or ((año % 4 == 0) and (año % 100 == 0) and (año%400 == 0)):
         print(f"El año {año} es bisiesto")
     else: 
-	    print(f"El año {año} no es bisiesto")
+	    print(f"El año {año} no es bisiesto") 
